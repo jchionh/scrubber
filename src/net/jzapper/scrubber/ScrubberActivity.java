fbab39401fc0c405fdd20525523706c9de93fccc
Jason Chionh <jasonchionh@gmail.com>
2014-05-24 14:06:59 +0800

    making the layout to overlay android view onto the GLSurfaceView

--- src/net/jzapper/scrubber/ScrubberActivity.java
+++ src/net/jzapper/scrubber/ScrubberActivity.java
@@ -2,6 +2,7 @@ package net.jzapper.scrubber;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import net.jzapper.scrubber.gfx.GLES20View;
@@ -22,17 +23,20 @@ public class ScrubberActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
 
         // make it full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        setContentView(R.layout.main);
+
         // setup our view and renderer
         view = new GLES20View(this);
         renderer = new GLRenderer(this);
         view.setRenderer(renderer);
-        setContentView(view);
+        //setContentView(view);
+
+        ((ViewGroup) findViewById(R.id.glsurface_container)).addView(view);
 
         //testSprite.setPos(250.0f, -250.0f, 0.0f);
         //sprites.add(testSprite);
